7981398b688b28f32b761667e219ba1a1f438348
Your Name <you@example.com>
2018-08-01 15:11:10 -0400

    Done

--- lib/models/CommandLineInterface.rb
+++ lib/models/CommandLineInterface.rb
@@ -5,18 +5,48 @@ end
 
 
 def gets_user_input
-  puts "What's your limit? "
+  puts "Enter 1 to search for walkers. Enter 2 to add dog. "
   answer= gets.chomp
 end
 
 
-def search(answer)
-  xall = []
-   Walker.all.select do |walker|
-   walker.price == answer.to_i
-   xall << walker.name
+def walk_people(name)
+  r = Walker.all.select do |people|
+    people.name == name
+  end
+  r.map do |walker|
+  walker.name
+
 end
-puts xall.uniq
+end
+
+
+def people_walkers
+  walks = []
+  Walker.all.each do |x|
+    walks << x.name
+  end
+  walks.uniq
+end
+
+def search(answer)
+  if answer.to_i == 1
+    puts "Enter name"
+    a = gets.chomp
+    puts walk_people(a)
+
+  elsif answer.to_i == 2
+      puts "Enter Dog name"
+      ans = gets.chomp
+      puts "Enter breed"
+      ans1 = gets.chomp
+    puts "Has been walked? Enter true or false"
+      ans2 = gets.chomp
+      puts "Your dog has been added to the waitlist!"
+    else
+      puts "Enter the correct number"
+
+    end
 end
 
 def finished
@@ -29,6 +59,7 @@ def finished
     run
   end
 end
+
 
 def run
   greet
